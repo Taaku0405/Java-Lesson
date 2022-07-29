e6ed61fbc4629fb71b431eab6d2cd216d9b38a0a
Taaku0405 <rakosutaa@gmail.com>
2022-07-29 16:41:32 +0900

    Java学習

--- Main.java
+++ Main.java
@@ -21,5 +21,40 @@ class Main {
 
     // 8を3で割った時の余りの値を出力
     System.out.println(8 % 3);
+
+    // "こんにちは"と"世界"を連結して出力
+    System.out.println("こんにちは" + "世界");
+
+    // "38"と"19"を連結して出力
+    System.out.println("38" + "19");
+
+    // 38と19を足して出力
+    System.out.println(38 + 19);
+
+    // int型の変数にnumberを定義して３を代入して出力
+    int number;
+    number = 3;
+    System.out.println(number);
+
+    // String型の変にnameを定義して"Wanko"を代入して出力
+    String name;
+    name = "Wanko";
+    System.out.println(name);
+
+    // 変数発展編
+    int number1 = 3;
+    int number2 = 7;
+    System.out.println(number1 + 5);
+    System.out.println(number1 * number2);
+
+    String text = "Javaを勉強中";
+    System.out.println("楽しく" + text);
+
+    // 変数の値を更新
+    number1 = 5;
+    text = "Java学習";
+    System.out.println(number1);
+    System.out.println(text);
+
   }
 }
