e66a97f87c8d2a35a1ec7a89414492e30e1d1cdf
Taaku0405 <rakosutaa@gmail.com>
2022-07-29 15:58:47 +0900

    Java課題復習

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    String msg = "";
-    msg += "Hello ";
-    msg += "World!";
-    System.out.println(msg);
+    // Javaの基本を覚えるための練習
+    System.out.println("Hello Java");
+
+    // 数値と文字列の違いを学ぶ
+    // 数値の17を出力
+    System.out.println(17);
+
+    // 5に3を足した値を出力
+    System.out.println(5 + 3);
+
+    // [5 + 3]を文字列で出力
+    System.out.println("5 + 3");
   }
 }
