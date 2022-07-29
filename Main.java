3f3983da359c30ebfb7fb55a0395120e66428a04
Taaku0405 <rakosutaa@gmail.com>
2022-07-29 16:05:03 +0900

    Java課題復習

--- Main.java
+++ Main.java
@@ -12,5 +12,14 @@ class Main {
 
     // [5 + 3]を文字列で出力
     System.out.println("5 + 3");
+
+    // 12を3で割った値を出力
+    System.out.println(12 / 3);
+
+    // 3に6を掛けた値を出力
+    System.out.println(3 * 6);
+
+    // 8を3で割った時の余りの値を出力
+    System.out.println(8 % 3);
   }
 }
